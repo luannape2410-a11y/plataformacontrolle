8d0023ef171ba7455b2fd4e42c348a6258e777fc
luannape2410-a11y <luanna.pe2410@gmail.com>
2026-04-23 14:48:39 -0300

    Add login route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter basename="/plataformacontrolle">
         <Routes>
+          <Route path="/login" element={<Login />} />
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
