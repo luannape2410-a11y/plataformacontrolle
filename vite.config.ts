36848aa25c5d467f37e40fa486dfe79025247fa5
luannape2410-a11y <luanna.pe2410@gmail.com>
2026-04-23 18:08:13 -0300

    Simplify Vite config by removing componentTagger
    Removed componentTagger from plugins in development mode.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: '/plataformacontrolle/",
   server: {
@@ -13,7 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()], 
   base: '/plataformacontrolle/',
   resolve: {
     alias: {
