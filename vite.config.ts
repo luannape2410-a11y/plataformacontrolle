922f8590f1136af2d5f74432a70d4a5ca2dc3dba
luannape2410-a11y <246407770+luannape2410-a11y@users.noreply.github.com>
2026-04-24 11:51:37 -0300

    Remove trailing slash from base URL in config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: '/plataformacontrolle/',
+  base: "/plataformacontrolle",
   plugins: [react()],
   resolve: {
     alias: {
