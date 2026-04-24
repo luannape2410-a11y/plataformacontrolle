b9a98d28a64964f9aea0b74c76c612116889c3a5
luannape2410-a11y <luanna.pe2410@gmail.com>
2026-04-24 08:21:33 -0300

    Add TooltipProvider and Toaster components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
@@ -7,13 +10,17 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
     <BrowserRouter basename="/plataformacontrolle">
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  </QueryClientProvider>
+  </TooltipProvider>
+</QueryClientProvider>
 );
 
 export default App;
